d502e039d42dda813f4661c1c808609bd0f64f4c
Chenglong Wang <ryuu.j.ching@gmail.com>
2018-09-20 17:53:21 +0900

    Add save_sourcecode and plot_confusion_matrix functions. Add save_sourcecode confirm to click.confirmation.

--- utils_cw/click.py
+++ utils_cw/click.py
@@ -1,22 +1,26 @@
 import os
-import click
+import sys
 import json
 import subprocess
 from termcolor import colored
+import click as cli
+from utils import save_sourcecode
 
 def print_smi(ctx, param, value):
     if value: 
         subprocess.call(["nvidia-smi"])
 
 def confirmation(ctx, param, value):
-    if click.confirm(colored('Continue processing with these params?\n{}\n'.format(json.dumps(ctx.params,indent=2)), color='cyan'), default=True, abort=True):
+    if cli.confirm(colored('Continue processing with these params?\n{}\n'.format(json.dumps(ctx.params,indent=2)), color='cyan'), default=True, abort=True):
         with open( os.path.join(ctx.params['output_dir'],'param.list'),'w') as f:
             json.dump(ctx.params, f, indent=2)
+    if cli.confirm(colored('Save source code?\n{}'.format(os.path.dirname(sys.argv[0])), color='cyan'), default=True): 
+        save_sourcecode(os.path.dirname(sys.argv[0]), ctx.params['output_dir']) #better use os.path.abspath(sys.argv[0])?
 
 def output_dir_check(ctx, param, value):
     from .utils import check_dir
     if os.path.isdir(value):
         return value
     else:
-        if click.confirm('Output dir not exists! Do you want to create new one?', default=True, abort=True):
+        if cli.confirm('Output dir not exists! Do you want to create new one?', default=True, abort=True):
             return check_dir(value)
--- utils_cw/utils.py
+++ utils_cw/utils.py
@@ -31,7 +31,7 @@ def recursive_glob2(searchroot='.', searchstr1='', searchstr2=''):
     """
     recursive glob with two search keywords
     """
-    
+
     if not os.path.isdir(searchroot):
         raise ValueError('No such directory: {}'.format(searchroot))
     print("search for {} and {} in {}".format(searchstr1,searchstr2,searchroot))
@@ -49,7 +49,7 @@ def recursive_glob3(searchroot='.', searchstr_list=None, excludestr_list=None, v
     """
     if not os.path.isdir(searchroot):
         raise ValueError('No such directory: {}'.format(searchroot))
-    
+
     if searchstr_list is None:
         raise ValueError('Search keyword list is empty!')
 
@@ -60,13 +60,13 @@ def recursive_glob3(searchroot='.', searchstr_list=None, excludestr_list=None, v
     for rootdir, dirnames, filenames in os.walk(searchroot):
         for incl in searchstr_list:
             filenames = [fname for fname in filenames if incl in fname]
-        for fname in filenames: 
+        for fname in filenames:
             f.append(os.path.join(rootdir, fname))
 
     if excludestr_list is not None:
         for ex in excludestr_list:
             f = [fname for fname in f if ex not in fname]
-                
+
     f.sort()
     return f
 
@@ -89,5 +89,65 @@ def PrintProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, l
         bar = fill * filledLength + '-' * (length - filledLength)
         print('\r%s [%s] %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
         # Print New Line on Complete
-        if iteration == total: 
+        if iteration == total:
             print()
+
+def save_sourcecode(code_rootdir, out_dir, file_type='*.py', verbose=True):
+    if not os.path.isdir(code_rootdir):
+        raise FileNotFoundError('Code root dir not exists! {}'.format(code_rootdir))
+    Print('Backup source code under root_dir:', code_rootdir, color='red', verbose=verbose)
+    outpath = check_dir(out_dir, 'source_code.tar', isFile=True)
+    tar_opt = 'cvf' if verbose else 'cf'
+    os.system("find {} -name '{}' | tar -{} {} -T -".format(code_rootdir, file_type, tar_opt, outpath))
+
+def plot_confusion_matrix(y_true, y_pred,
+                          filename, labels,
+                          ymap=None, figsize=(10, 10),
+                          true_axis_label='Ground-truth',
+                          pred_axis_label='Preditions'):
+    """
+    Generate matrix plot of confusion matrix with pretty annotations.
+    The plot image is saved to disk.
+    args: 
+      y_true:    true label of the data, with shape (nsamples,)
+      y_pred:    prediction of the data, with shape (nsamples,)
+      filename:  filename of figure file to save
+      labels:    string array, name the order of class labels in the confusion matrix.
+                 use `clf.classes_` if using scikit-learn models. with shape (nclass,).
+      ymap:      dict: any -> string, length == nclass.
+                 if not None, map the labels & ys to more understandable strings.
+                 Caution: original y_true, y_pred and labels must align.
+      figsize:   the size of the figure plotted.
+    """
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from sklearn.metrics import confusion_matrix
+
+    if ymap != None:
+        y_pred = [ymap[yi] for yi in y_pred]
+        y_true = [ymap[yi] for yi in y_true]
+        labels = [ymap[yi] for yi in labels]
+    cm = confusion_matrix(y_true, y_pred, labels=labels)
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index=labels, columns=labels)
+    cm.index.name = true_axis_label
+    cm.columns.name = pred_axis_label
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(cm, annot=annot, fmt='', ax=ax)
+    plt.savefig(filename)
