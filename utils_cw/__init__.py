b11590dac090b66708e7db2ff0c42ef2f0f7a432
ChenglongWang <ryuu.j.ching@gmail.com>
2021-02-02 13:42:44 +0800

    Update __init__.py
    Remove augmentations.

--- utils_cw/__init__.py
+++ utils_cw/__init__.py
@@ -4,6 +4,6 @@ assert sys.version_info >= (3, 6), "Python ver. >=3.6 is required!"
 from .proc import *
 from .mori import *
 from .logger import Logger
-from .augmentations import *
+# from .augmentations import *
 from .utils import *
 from .click import *
